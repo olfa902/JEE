dfca72d78a1d521b92258d86ead829a41bb8a1c0
yathereb <olfawaslati@gmail.com>
2020-03-22 13:37:08 +0100

    First commit

---
+++ tp1/src/isetj/servlets/SearchServlet.java
@@ -0,0 +1,52 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String t =request.getParameter("t");
+    	String rech=request.getParameter("rech");
+		if (rech.equals("yahoo"))
+		{
+			response.sendRedirect("http://www.google.com/search?q="+rech);
+		} 
+		else {
+		    response.sendRedirect("http://search.yahoo.com/search?p="+rech);
+		    }
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
